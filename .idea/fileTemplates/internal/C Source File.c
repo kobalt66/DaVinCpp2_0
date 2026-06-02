d516f7d2f7571333cd1f37e6896c3324cb1c5f3b
lephobix <lephobix@gmail.com>
2026-06-02 21:10:53 +0200

    Deleted useless IDE files

--- .idea/fileTemplates/internal/C Source File.c
+++
@@ -1,6 +0,0 @@
-#[[#include]]# "${HEADER_FILENAME}"
-
-namespace davincpp
-{
-    
-}
--- .idea/fileTemplates/internal/C++ Class Header.h
+++
@@ -1,9 +0,0 @@
-#[[#pragma]]# once
-
-namespace davincpp
-{
-    class ${NAME} final
-    {
-    public:
-    };
-}
